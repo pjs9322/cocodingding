d7cf054b96e6e18dc8440c8dc73c391e0942b9fc
pjs9322 <pjs9322@gmail.com>
2018-10-26 15:11:14 +0900

    0.0.2 commit

---
+++ cocodingding_0.0.2/src/main/java/com/spring/qboard/BoardController.java
@@ -0,0 +1,72 @@
+package com.spring.qboard;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.qboard.service.BoardService;
+import com.spring.qboard.vo.BoardVO;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class BoardController {
+	
+	@Resource(name = "boardService")
+	private BoardService boardService;
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	
+	/**
+	 *  게시판 목록
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/boardList.do", method = RequestMethod.GET)
+	public String boardList(Model model) throws Exception {
+
+	    List<BoardVO> list = boardService.selectBoardList();
+
+	    logger.info(list.toString());
+
+	    model.addAttribute("list", list);
+
+	    return "boardList";
+	}
+	
+	/**
+	 * 게시판 등록폼
+	 * @param boardVO
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/boardRegisterForm.do", method = RequestMethod.GET)
+	public String boardRegisterForm(@ModelAttribute("boardVO") BoardVO boardVO, Model model) throws Exception {
+
+	    return "boardRegisterForm";
+	}
+	
+	/**
+	 * 글을 등록합니다.
+	 * @return
+	 */
+	@RequestMapping(value = "/boardInsert.do", method = RequestMethod.POST)
+	public String boardInsert(@ModelAttribute("boardVO") BoardVO boardVO, Model model) {
+		
+		boardService.insertBoard(boardVO);
+		
+		return "redirect:/boardList.do";
+	}
+}
