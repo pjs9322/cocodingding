27b5051110eab645ee15fd82a30e830d47d372b6
JungHyunsong1903 <adfhjl123@naver.com>
2018-10-31 15:58:34 +0900

    update 0.6.3

---
+++ cocodingding_0.6.3/java/com/spring/qboard/service/MemberService.java
@@ -0,0 +1,24 @@
+package com.spring.qboard.service;
+
+import java.util.List;
+
+import com.spring.qboard.vo.MemberVO;
+
+
+public interface MemberService {
+	
+	// 전체 유저 목록 보기
+	public List<MemberVO> selectMemberList() throws Exception;
+
+	// 신규 유저 회원가입
+	public void insertMember(MemberVO memberVO) throws Exception;
+	
+	// 유저 개인정보 보기
+	public MemberVO selectMember(String id) throws Exception;
+	
+	// 유저 개인정보 수정
+	public void updateMember(MemberVO memberVO) throws Exception;
+	
+	// 유저 탈퇴
+	public void deleteMember(String id) throws Exception;
+}
