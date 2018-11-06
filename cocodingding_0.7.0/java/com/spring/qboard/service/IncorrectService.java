89c63b248d13f4ad17e4eccd38bb84d64df2bc90
JungHyunsong1903 <adfhjl123@naver.com>
2018-11-06 18:35:59 +0900

    commit 0.7.0 change board_View

---
+++ cocodingding_0.7.0/java/com/spring/qboard/service/IncorrectService.java
@@ -0,0 +1,22 @@
+package com.spring.qboard.service;
+
+import java.util.List;
+
+import com.spring.qboard.vo.IncorrectVO;
+
+
+public interface IncorrectService {
+	
+	// 전체 장바구니 목록 보기
+	public List<IncorrectVO> selectIncorrectList() throws Exception;
+
+	// 유저 장바구니 추가
+	public void insertIncorrect(IncorrectVO incorrectVO) throws Exception;
+	
+	// 유저별 장바구니 보기
+	public List<IncorrectVO> selectIncorrect(String id) throws Exception;
+	
+	// 유저 장바구니 삭제
+	public void deleteIncorrect(IncorrectVO incorrectVO) throws Exception;
+	
+}
